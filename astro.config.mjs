30899e0cc1cfe4c214666efae5054b49c61815d3
Nate Duff <nate@duffcloudservices.com>
2026-01-08 06:20:36 -0500

    Batman

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import dcs from '@duffcloudservices/cms-astro'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://demo.duffcloudservices.com',
+  integrations: [dcs()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  output: 'static',
+  build: {
+    assets: '_assets'
+  }
+})
